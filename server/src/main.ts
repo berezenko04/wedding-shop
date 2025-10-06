5e04d87046d0aa7cf20e5450a332da6424539926
berezenko04 <koffld@yahoo.com>
2025-10-06 13:26:40 +0300

    feat: added validation pipe, api prefix, cookie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser'
+import * as express from 'express'
+
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+// modules
+import { AppModule } from './app.module';
+
+// services
 import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +29,17 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true
   })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
