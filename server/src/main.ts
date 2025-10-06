da5ebb3e201773e30342417c86393838c977fd1f
berezenko04 <koffld@yahoo.com>
2025-10-06 13:17:59 +0300

    feat: enabled cors, added helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService)
+
+  app.use(helmet());
+
+  const corsEnv = configService.get<string>('FRONTEND_URL');
+  const origins = corsEnv
+    ? corsEnv.split(',').map(s => s.trim())
+    : ['http://localhost:5173'];
+
+  app.enableCors({
+    origin: origins,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
