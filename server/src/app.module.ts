2b3fd56357f12428e9c92067810a33a2f170aee1
berezenko04 <koffld@yahoo.com>
2025-10-06 15:23:26 +0300

    feat: added logs model, added logging service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [ 
@@ -12,7 +13,8 @@ import { PrismaModule } from './prisma/prisma.module';
         '.env',
       ],
     }),
-    PrismaModule
+    PrismaModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
