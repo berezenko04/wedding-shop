557ff1bd00b0a4d6e5a78bb80ab3e53a9956aa62
berezenko04 <koffld@yahoo.com>
2025-10-06 14:53:38 +0300

    Revert "Merge branch 'feature/configure-project' into develop"
    This reverts commit 70956b58dc931086696db48bcc5d1ae4503811b3, reversing
    changes made to 1501651356a17164b7d2ee0d84d63f32e9fb37a5.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ 
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [
-        `.env.${process.env.NODE_ENV || 'development'}`,
-        '.env.development',
-        '.env',
-      ],
-    }),
-    PrismaModule
-  ],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,50 +1,8 @@
-import helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
-import * as requestIp from 'request-ip';
-import * as express from 'express';
-
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-
-// modules
 import { AppModule } from './app.module';
-
-// services
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const configService = app.get(ConfigService);
-
-  app.use(helmet());
-
-  const corsEnv = configService.get<string>('FRONTEND_URL');
-  const origins = corsEnv
-    ? corsEnv.split(',').map((s) => s.trim())
-    : ['http://localhost:5173'];
-
-  app.enableCors({
-    origin: origins,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-    }),
-  );
-
-  app.setGlobalPrefix('api/v1');
-
-  app.use(cookieParser());
-  app.use(requestIp.mw());
-
-  app.use(express.json({ limit: '500kb' }));
-  app.use(express.urlencoded({ limit: '500kb', extended: true }));
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
