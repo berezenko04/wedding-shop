171a028f2c9cc01bd4bfdcf00bfe6b5c0ac0aa0f
berezenko04 <koffld@yahoo.com>
2025-10-06 15:50:25 +0300

    feat: configured jwt module in auth module

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+
+// services
 import { AuthService } from './auth.service';
+
+// controllers
 import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_ACCESS_SECRET'),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
