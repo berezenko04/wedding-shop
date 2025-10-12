abb21cc132248e73ffd5b825802ff400a71ced5b
berezenko04 <koffld@yahoo.com>
2025-10-12 17:06:40 +0300

    refactor: changed output from get all payment methods of user

--- server/src/modules/payment/payment.service.ts
+++ server/src/modules/payment/payment.service.ts
@@ -64,16 +64,6 @@ export class PaymentService {
   async all(userId: string) {
     return this.prisma.payment.findMany({
       where: { userId },
-      select: {
-        id: true,
-        method: true,
-        primary: true,
-        email: true,
-        cardNumber: true,
-        cardExp: true,
-        cardCvv: true,
-        cardHolder: true,
-      },
     });
   }
 
