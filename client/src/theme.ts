1e0c4f0cf4e4deddd30ac41fe8ba1bdcd22220f6
berezenko04 <koffld@yahoo.com>
2025-10-17 17:09:56 +0100

    feat: added styles to typography and headings

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -23,6 +23,36 @@ const theme = createTheme({
       black: "#121212",
     },
   },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          "&.MuiTypography-h1, &.MuiTypography-h2, &.MuiTypography-h3, &.MuiTypography-h4, &.MuiTypography-h5, &.MuiTypography-h6":
+            {
+              color: theme.palette.grey[700],
+              fontWeight: 500,
+              letterSpacing: "-2.5%",
+            },
+        }),
+        h1: {
+          fontSize: 80,
+        },
+        h2: {
+          fontSize: 56,
+        },
+        h3: {
+          fontSize: 48,
+        },
+        h4: {
+          fontSize: 32,
+        },
+        h5: {
+          fontSize: 24,
+        },
+        body1: { fontSize: 16, lineHeight: 24 },
+      },
+    },
+  },
 });
 
 export default theme;
