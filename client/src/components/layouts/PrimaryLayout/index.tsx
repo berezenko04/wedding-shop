07892b14620f08761a06a08d2cb29abe847d968b
berezenko04 <koffld@yahoo.com>
2025-10-20 11:32:30 +0100

    feat: added primary layout component

--- client/src/components/layouts/PrimaryLayout/index.tsx
+++ client/src/components/layouts/PrimaryLayout/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+// components
+import Header from "@/components/navigation/Header";
+import CustomContainer from "@/components/ui/layout/CustomContainer";
 
 const PrimaryLayout: React.FC = () => {
   return (
-    <div>PrimaryLayout</div>
-  )
-}
+    <Stack sx={{ minHeight: "100svh" }}>
+      <Header />
+      <CustomContainer sx={{ flex: 1, display: "flex", flexDirection: "column", py: { xs: 3, md: 6 } }}>
+        <Outlet />
+      </CustomContainer>
+    </Stack>
+  );
+};
 
-export default PrimaryLayout
+export default PrimaryLayout;
