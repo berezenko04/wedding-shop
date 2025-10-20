e023b60d6d094dbe8fab7103ad3c6f281535e5aa
berezenko04 <koffld@yahoo.com>
2025-10-20 10:57:15 +0100

    feat: added icons material library

--- client/src/components/navigation/Header/index.tsx
+++ client/src/components/navigation/Header/index.tsx
@@ -1,13 +1,15 @@
-import { Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 
 // components
 import Logo from "../Logo";
 
 const Header: React.FC = () => {
   return (
-    <Stack component="header" gap={3} justifyContent="space-between" alignItems="center">
+    <Stack component="header" flexDirection="row" gap={3} justifyContent="space-between" alignItems="center">
       <Logo />
-      
+      <Button  size="small" color="grey" variant="outlined" sx={{ textTransform: "none" }}>
+        Catalog
+      </Button>
     </Stack>
   );
 };
