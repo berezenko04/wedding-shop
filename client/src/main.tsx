1dc72a0920fb1eff34ee9e44abed28f592f35ebd
berezenko04 <koffld@yahoo.com>
2025-10-21 10:40:22 +0100

    chore: implemented redux provider to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import App from "./App.tsx";
 
 // theme
 import theme from "./theme.ts";
 
+// store
+import { store } from "./redux/store.ts";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-      <Toaster position="top-center" toastOptions={{ style: { maxWidth: 600 } }} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+        <Toaster position="top-center" toastOptions={{ style: { maxWidth: 600 } }} />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
