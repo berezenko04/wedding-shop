9867ddff7ac2d0ce0401bfeeac3fc8aa7d1d0773
berezenko04 <koffld@yahoo.com>
2025-10-22 21:56:57 +0100

    style: removed unusable import from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
