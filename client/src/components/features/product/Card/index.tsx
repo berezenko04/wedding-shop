6b933232dac66cac39d28b602a841c9dab83d327
berezenko04 <koffld@yahoo.com>
2025-10-22 10:39:00 +0100

    chore: moved discount label to component

--- client/src/components/features/product/Card/index.tsx
+++ client/src/components/features/product/Card/index.tsx
@@ -1,5 +1,8 @@
 import { Box, Link, Stack, Typography } from "@mui/material";
 
+// components
+import DiscountLabel from "../DiscountLabel";
+
 // types
 import { Product } from "@/api/products/products.types";
 
@@ -12,34 +15,7 @@ const Card: React.FC<Product> = ({ posterUrl, title, price, slug, discount }) =>
           src={posterUrl}
           sx={{ height: "100%", width: "100%", objectFit: "cover", objectPosition: "center" }}
         />
-        {discount > 0 && (
-          <Box
-            sx={{
-              position: "absolute",
-              bottom: 40,
-              right: -40,
-              width: 200,
-              display: "flex",
-              justifyContent: "center",
-              transform: "rotate(-45deg)",
-              zIndex: 2,
-              pointerEvents: "none",
-            }}
-          >
-            <Box
-              sx={{
-                backgroundColor: "success.main",
-                px: 12,
-                py: 1.5,
-                boxShadow: 2,
-              }}
-            >
-              <Typography variant="medium" color="common.white" fontWeight={600} whiteSpace="nowrap">
-                {Math.round(discount * 100)}% OFF
-              </Typography>
-            </Box>
-          </Box>
-        )}
+        {discount > 0 && <DiscountLabel discount={discount} />}
       </Box>
 
       <Stack gap={1}>
