7dbf8ef5ae8cea7c687c75cbf512a29291bef20b
berezenko04 <koffld@yahoo.com>
2025-10-22 10:56:34 +0100

    feat: added variants to product card

--- client/src/components/features/product/Card/index.tsx
+++ client/src/components/features/product/Card/index.tsx
@@ -2,12 +2,16 @@ import { Box, Link, Stack, Typography } from "@mui/material";
 
 // components
 import DiscountLabel from "../DiscountLabel";
+import AddToWishlistButton from "@/components/ui/buttons/AddToWishlist";
 
 // types
 import { Product } from "@/api/products/products.types";
-import AddToWishlistButton from "@/components/ui/buttons/AddToWishlist";
 
-const Card: React.FC<Product> = ({ posterUrl, title, price, slug, discount }) => {
+interface CardProps extends Product {
+  variant?: "catalog" | "default";
+}
+
+const Card: React.FC<CardProps> = ({ posterUrl, title, price, slug, discount, variant = "default" }) => {
   return (
     <Stack gap={2} component={Link} href={`/catalog/${slug}`}>
       <Box position="relative" overflow="hidden" sx={{ height: 535 }}>
@@ -21,18 +25,28 @@ const Card: React.FC<Product> = ({ posterUrl, title, price, slug, discount }) =>
       </Box>
 
       <Stack gap={1}>
-        <Typography variant="medium">{title}</Typography>
-        <Stack flexDirection="row" alignItems="center" gap={2}>
-          <Typography variant="medium" color="primary.main">
-            {discount ? (price - price * discount).toFixed(2) : price.toFixed(2)} USD
-          </Typography>
-
-          {discount > 0 && (
-            <Typography variant="medium" color="grey.400" sx={{ textDecoration: "line-through" }}>
-              {price.toFixed(2)} USD
+        <Stack flexDirection="row" alignItems="center" justifyContent="space-between" gap={2}>
+          <Typography variant="medium">{title}</Typography>
+          {variant === "default" && (
+            <Typography variant="medium" color="common.black">
+              {(price - price * discount).toFixed(2)} USD
             </Typography>
           )}
         </Stack>
+
+        {variant === "catalog" && (
+          <Stack flexDirection="row" alignItems="center" gap={2}>
+            <Typography variant="medium" color="primary.main">
+              {discount ? (price - price * discount).toFixed(2) : price.toFixed(2)} USD
+            </Typography>
+
+            {discount > 0 && (
+              <Typography variant="medium" color="grey.400" sx={{ textDecoration: "line-through" }}>
+                {price.toFixed(2)} USD
+              </Typography>
+            )}
+          </Stack>
+        )}
       </Stack>
     </Stack>
   );
