0e0c6c5c71d103882a00fd92242115c781bc1dce
berezenko04 <koffld@yahoo.com>
2025-10-26 22:05:37 +0000

    feat: added and connected to router catalog product page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,16 +9,17 @@ import AuthLayout from "./components/layouts/AuthLayout";
 // redux
 import { refresh } from "./redux/auth/auth.actions";
 
-const RegisterPage = lazy(() => import("@/pages/Register"));
-const LoginPage = lazy(() => import("@/pages/Login"));
-const ForgotPasswordPage = lazy(() => import("@/pages/ForgotPassword"));
-const VerifyOtpPage = lazy(() => import("@/pages/VerifyOtp"));
-const VerifyOtpSuccessPage = lazy(() => import("@/pages/VerifyOtpSuccess"));
-const ResetPasswordPage = lazy(() => import("@/pages/ResetPassword"));
-const ResetPasswordSuccessPage = lazy(() => import("@/pages/ResetPasswordSuccess"));
+const RegisterPage = lazy(() => import("@/pages/auth/Register"));
+const LoginPage = lazy(() => import("@/pages/auth/Login"));
+const ForgotPasswordPage = lazy(() => import("@/pages/auth/ForgotPassword"));
+const VerifyOtpPage = lazy(() => import("@/pages/auth/VerifyOtp"));
+const VerifyOtpSuccessPage = lazy(() => import("@/pages/auth/VerifyOtpSuccess"));
+const ResetPasswordPage = lazy(() => import("@/pages/auth/ResetPassword"));
+const ResetPasswordSuccessPage = lazy(() => import("@/pages/auth/ResetPasswordSuccess"));
 
 const HomePage = lazy(() => import("@/pages/Home"));
 const CatalogPage = lazy(() => import("@/pages/Catalog"));
+const CatalogProductPage = lazy(() => import("@/pages/CatalogProduct"));
 const NotFoundPage = lazy(() => import("@/pages/NotFound"));
 
 function App() {
@@ -44,7 +45,7 @@ function App() {
         <Route element={<PrimaryLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:slug" />
+          <Route path="/catalog/:slug" element={<CatalogProductPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
 
---
+++ client/src/pages/CatalogProduct/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CatalogProduct: React.FC = () => {
+  return (
+    <div>CatalogProduct</div>
+  )
+}
+
+export default CatalogProduct
