fd385b2cc75f5eb1e41fd4ca055fb13e799c778f
berezenko04 <koffld@yahoo.com>
2025-10-27 09:59:22 +0000

    feat: added fetching product info by slug and basic layout of catalog product page

--- client/src/components/features/product/Info/index.tsx
+++ client/src/components/features/product/Info/index.tsx
@@ -3,7 +3,18 @@ import { Stack, Typography } from "@mui/material";
 // components
 import ProductPrice from "../Price";
 
-const ProductInfo: React.FC = ({ title, price, discount, description }) => {
+// types
+import { Sizes } from "@/types/enums.types";
+
+type ProductInfoProps = {
+  title: string;
+  price: number;
+  discount: number;
+  description: string;
+  sizes: Sizes;
+};
+
+const ProductInfo: React.FC<ProductInfoProps> = ({ title, price, discount, description }) => {
   return (
     <Stack gap={4} px={8}>
       <Stack gap={1}>
--- client/src/pages/CatalogProduct/index.tsx
+++ client/src/pages/CatalogProduct/index.tsx
@@ -1,15 +1,31 @@
 import { Grid, Stack } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
 
 // components
 import ProductInfo from "@/components/features/product/Info";
 
+// api
+import ProductsService from "@/api/products/products.service";
+
 const CatalogProduct: React.FC = () => {
+  const { slug } = useParams<{ slug: string }>();
+
+  const { data } = useQuery({
+    queryKey: ["product", slug],
+    queryFn: async () => {
+      if (!slug) throw new Error("Slug is not provided");
+      return ProductsService.get(slug);
+    },
+    enabled: !!slug,
+  });
+
   return (
     <Stack>
       <Grid container spacing={4}>
         <Grid size={{ xs: 6 }}></Grid>
         <Grid size={{ xs: 6 }}>
-          <ProductInfo title="Angle" price={2000} discount={0.2} />
+          <ProductInfo {...data} />
         </Grid>
       </Grid>
     </Stack>
