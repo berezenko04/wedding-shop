c87cace235cc86385a0beaade466da39a6909006
berezenko04 <koffld@yahoo.com>
2025-10-30 13:18:06 +0000

    chore: implemented fetch wishlist in header

--- client/src/components/navigation/Header/index.tsx
+++ client/src/components/navigation/Header/index.tsx
@@ -14,9 +14,16 @@ import { authSelector } from "@/redux/auth/auth.selectors";
 
 // api
 import CartService from "@/api/cart/cart.service";
+import WishlistService from "@/api/wishlist/wishlist.service";
 
 // icons
 import { FavoriteBorderOutlined, LocalMallOutlined, PersonOutline, StorefrontOutlined } from "@mui/icons-material";
+
+// types
+import { GetAllWishlist } from "@/api/wishlist/wishlist.types";
+
+// constants
+import { PAGE_LIMIT } from "@/constants";
 
 const Header: React.FC = () => {
   const { isAuth } = useSelector(authSelector);
@@ -27,6 +34,14 @@ const Header: React.FC = () => {
     queryKey: ["cart"],
     queryFn: async () => await CartService.getAll(),
     staleTime: Infinity,
+    enabled: isAuth,
+  });
+
+  const { data: wishlist } = useQuery<GetAllWishlist>({
+    queryKey: ["wishlist"],
+    queryFn: async () => await WishlistService.getAll({ page: 1, limit: PAGE_LIMIT }),
+    staleTime: Infinity,
+    enabled: isAuth,
   });
 
   return (
@@ -48,7 +63,9 @@ const Header: React.FC = () => {
           {isAuth ? (
             <Stack flexDirection="row" alignItems="center" gap={0.5}>
               <IconButton href="/profile/wishlist">
-                <FavoriteBorderOutlined />
+                <Badge color="primary" badgeContent={wishlist?.total}>
+                  <FavoriteBorderOutlined />
+                </Badge>
               </IconButton>
               <IconButton onClick={() => setIsCartOpened(true)}>
                 <Badge color="primary" badgeContent={cart.reduce((acc, i) => acc + i.quantity, 0)}>
