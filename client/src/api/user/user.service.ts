d5a6ac817cc6b46c6de78e0c9cca7bb5cce631f4
berezenko04 <koffld@yahoo.com>
2025-11-03 10:03:31 +0000

    feat: added user service and routes

---
+++ client/src/api/user/user.service.ts
@@ -0,0 +1,23 @@
+import { httpGet, httpPatch } from "@/middlewares/axios.middleware";
+
+// types
+import { UpdateUserBody, User, UserSession } from "./user.types";
+
+const R = {
+  users: "/users",
+  sessions: "/users/sessions",
+} as const;
+
+const UserService = {
+  async getMe() {
+    return httpGet<User>(R.users);
+  },
+  async getSessions() {
+    return httpGet<UserSession[]>(R.sessions);
+  },
+  async updateUser(body: UpdateUserBody) {
+    return httpPatch<User>(R.users, body);
+  },
+};
+
+export default UserService;
