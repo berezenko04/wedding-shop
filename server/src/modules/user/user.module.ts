f7b3ef231fc4dbaaf5d53128ead652420c16017e
berezenko04 <koffld@yahoo.com>
2025-11-07 10:26:35 +0000

    feat: added change password function to auth service

--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
