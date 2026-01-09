d08b92e8f4e9e2c536684dffa1d26e02cbb2a5fd
berezenko04 <koffld@yahoo.com>
2026-01-09 23:35:10 +0000

    refactor: update Home page links to use 'category' instead of 'sex' for product filtering

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -31,7 +31,7 @@ const Home: React.FC = () => {
               height={730}
               imgSrc="/categories-dresses.webp"
               linkText="Dresses"
-              linkHref="/catalog?sex=female"
+              linkHref="/catalog?category=dresses"
             />
           </Grid>
           <Grid size={{ xs: 6 }}>
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
               height={730}
               imgSrc="/categories-suits.webp"
               linkText="Suits"
-              linkHref="/catalog?sex=male"
+              linkHref="/catalog?category=suits"
             />
           </Grid>
         </Grid>
@@ -51,7 +51,7 @@ const Home: React.FC = () => {
               height={480}
               imgSrc="/clothing-for-her.webp"
               linkText="For her"
-              linkHref="/catalog?sex=female"
+              linkHref="/catalog?category=dresses"
             />
           </Grid>
           <Grid size={{ xs: 6 }}>
@@ -59,7 +59,7 @@ const Home: React.FC = () => {
               height={480}
               imgSrc="/clothing-for-him.webp"
               linkText="For him"
-              linkHref="/catalog?sex=male"
+              linkHref="/catalog?category=suits"
             />
           </Grid>
         </Grid>
