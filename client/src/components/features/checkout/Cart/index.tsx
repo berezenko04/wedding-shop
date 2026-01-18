149c0ff3af328ae867c6b8974ae1f9ce48fc823d
berezenko04 <koffld@yahoo.com>
2026-01-18 21:51:17 +0000

    feat: Implement checkout cart component and introduce global constants for pagination, reviews, and delivery costs.

--- client/src/components/features/checkout/Cart/index.tsx
+++ client/src/components/features/checkout/Cart/index.tsx
@@ -8,11 +8,18 @@ import CartItem from './Item';
 // types
 import { CartItem as CartItemType } from '@/api/cart/cart.types';
 
+// constants
+import { DELIVERY_COST } from '@/constants';
+
 const CheckoutCart: React.FC = () => {
   const queryClient = useQueryClient();
 
   const cart = queryClient.getQueryData<CartItemType[]>(['cart']) || [];
 
+  const total = cart.reduce(
+    (acc, item) => acc + item.product.price * (1 - (item?.product.discount ?? 0)) * item.quantity,
+    0,
+  );
   return (
     <OutlinedBlock flex={1} maxHeight="80vh">
       <Stack gap={4} flex={1}>
@@ -32,22 +39,22 @@ const CheckoutCart: React.FC = () => {
         <Stack gap={1}>
           <Stack flexDirection="row" justifyContent="space-between" gap={4}>
             <Typography>Total</Typography>
-            <Typography>8600 USD</Typography>
+            <Typography>{total.toFixed(2)} USD</Typography>
           </Stack>
-          <Stack flexDirection="row" justifyContent="space-between" gap={4}>
+          {/* <Stack flexDirection="row" justifyContent="space-between" gap={4}>
             <Typography>Discount</Typography>
             <Typography>20%</Typography>
-          </Stack>
+          </Stack> */}
           <Stack flexDirection="row" justifyContent="space-between" gap={4}>
             <Typography>Delivery</Typography>
-            <Typography>20 USD</Typography>
+            <Typography>{DELIVERY_COST} USD</Typography>
           </Stack>
           <Stack flexDirection="row" justifyContent="space-between" gap={4}>
             <Typography variant="medium" fontSize={24} textTransform="uppercase">
               Grand Total
             </Typography>
             <Typography variant="medium" fontSize={24} textTransform="uppercase">
-              8620 USD
+              {(total + DELIVERY_COST).toFixed(2)} USD
             </Typography>
           </Stack>
         </Stack>
