0a2d9abd7af8842f5af9b387e66e010b2642a451
berezenko04 <koffld@yahoo.com>
2026-01-19 14:05:52 +0000

    feat: Add ShippingAddress component to display the user's primary shipping address and personal details.

--- client/src/components/features/checkout/ShippingAddress/index.tsx
+++ client/src/components/features/checkout/ShippingAddress/index.tsx
@@ -1,15 +1,38 @@
 import { Stack, Typography } from '@mui/material';
+import { useQueryClient } from '@tanstack/react-query';
 
 // components
 import OutlinedBlock from '@/components/ui/layout/OutlinedBlock';
+import EditButton from '@/components/ui/buttons/Edit';
+
+// hooks
+import { useUserShippingAddresses } from '@/hooks/useUserShippingAddresses';
+
+//
+import { User } from '@/api/user/user.types';
 
 const ShippingAddress: React.FC = () => {
+  const queryClient = useQueryClient();
+
+  const { data: addresses } = useUserShippingAddresses();
+  const user = queryClient.getQueryData<User>(['user']);
+
+  const primaryAddress = addresses?.find((address) => address.primary);
+
   return (
-    <OutlinedBlock>
-      <Stack flexDirection="row" alignItems="center" gap={4}>
+    <OutlinedBlock sx={{ gap: 2 }}>
+      <Stack flexDirection="row" alignItems="center" justifyContent="space-between" gap={4}>
         <Typography variant="medium" textTransform="uppercase">
           Shipping Address
         </Typography>
+        <EditButton href="/profile/shipping-address" />
+      </Stack>
+      <Stack gap={1}>
+        <Typography>
+          {user?.firstName} {user?.lastName}
+        </Typography>
+        <Typography>{primaryAddress?.address}</Typography>
+        <Typography>{user?.email}</Typography>
       </Stack>
     </OutlinedBlock>
   );
