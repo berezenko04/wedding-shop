bf84e3b3e8fc113f7e4b5ad0cbd17786afa9644b
berezenko04 <koffld@yahoo.com>
2026-05-03 21:04:41 +0100

    Added WAITING_FOR_PAYMENT status in order Enum

--- server/src/modules/stripe/stripe.controller.ts
+++ server/src/modules/stripe/stripe.controller.ts
@@ -1,6 +1,12 @@
 import { Controller, Post } from '@nestjs/common';
+
+// services
 import { StripeService } from './stripe.service';
 import { CartService } from '../cart/cart.service';
+
+// decorators
+import { Auth } from '../auth/decorators/auth.decorator';
+import { User } from 'src/common/decorators/user.decorator';
 
 @Controller('stripe')
 export class StripeController {
@@ -9,6 +15,15 @@ export class StripeController {
     private readonly cartService: CartService,
   ) {}
 
+  @Auth()
   @Post('checkout')
-  async checkout() {}
+  async checkout(@User('id') userId: string) {
+    const cart = await this.cartService.getCart(userId);
+    const total = cart.reduce(
+      (acc, { quantity, product }) =>
+        acc + quantity * (product.price * (1 - (product.discount ?? 0))),
+      0,
+    );
+    return this.stripeService.createCheckoutSession();
+  }
 }
--- server/src/modules/stripe/stripe.module.ts
+++ server/src/modules/stripe/stripe.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+
+// services
 import { StripeService } from './stripe.service';
+
+// controllers
 import { StripeController } from './stripe.controller';
 
+// modules
+import { CartModule } from '../cart/cart.module';
+
 @Module({
+  imports: [CartModule],
   controllers: [StripeController],
   providers: [StripeService],
 })
