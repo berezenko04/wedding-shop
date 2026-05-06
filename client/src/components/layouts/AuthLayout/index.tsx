a62fc0f3bd35d1fbad32d4193ba1c6146af070ba
berezenko04 <koffld@yahoo.com>
2026-05-06 20:16:58 +0100

    Remove image block when under md

--- client/src/components/layouts/AuthLayout/index.tsx
+++ client/src/components/layouts/AuthLayout/index.tsx
@@ -1,28 +1,35 @@
-import { Box, Grid, Stack, Typography } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Box, Grid, Stack, Typography } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 
 const AuthLayout: React.FC = () => {
   return (
-    <Grid container sx={{ height: "100vh", minHeight: "100vh" }}>
-      <Grid size={{ xs: 6 }} sx={{ height: "100%" }}>
-        <Box sx={{ position: "relative", height: "100%", overflow: "hidden" }}>
+    <Grid container sx={{ height: '100vh', minHeight: '100vh' }}>
+      <Grid size={6} sx={{ display: { xs: 'none', md: 'grid' }, height: '100%' }}>
+        <Box sx={{ position: 'relative', height: '100%', overflow: 'hidden' }}>
           <Box
             component="img"
             src="/auth.webp"
             sx={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              objectPosition: "center",
-              display: "block",
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              objectPosition: 'center',
+              display: 'block',
             }}
           />
           <Stack
-            gap={3}
+            gap={{ xs: 1.5, lg: 3 }}
             alignItems="center"
-            sx={{ position: "absolute", width: "100%", bottom: 48, transform: "translateX(-50%)", left: "50%" }}
+            sx={{
+              position: 'absolute',
+              width: '100%',
+              bottom: { xs: 24, lg: 48 },
+              p: 2,
+              transform: 'translateX(-50%)',
+              left: '50%',
+            }}
           >
-            <Typography fontSize={40} fontWeight={600} color="common.white" textAlign="center">
+            <Typography fontSize={{ xs: 28, lg: 40 }} fontWeight={600} color="common.white" textAlign="center">
               Turn your ideas into reality
             </Typography>
             <Typography color="common.white" textAlign="center">
@@ -31,8 +38,8 @@ const AuthLayout: React.FC = () => {
           </Stack>
         </Box>
       </Grid>
-      <Grid size={{ xs: 6 }} sx={{ display: "flex", height: "100%" }}>
-        <Stack sx={{ flex: 1, height: "100%", alignItems: "center", justifyContent: "center" }}>
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: 'flex', height: '100%' }}>
+        <Stack sx={{ flex: 1, height: '100%', alignItems: 'center', justifyContent: 'center' }}>
           <Outlet />
         </Stack>
       </Grid>
