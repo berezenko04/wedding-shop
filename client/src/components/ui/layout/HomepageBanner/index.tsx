094a891879370f873d60761197e971efbd44a2fd
berezenko04 <koffld@yahoo.com>
2026-05-06 20:10:09 +0100

    Make responsive homepage banner

--- client/src/components/ui/layout/HomepageBanner/index.tsx
+++ client/src/components/ui/layout/HomepageBanner/index.tsx
@@ -6,7 +6,7 @@ import OutlinedWhiteArrowButton from '@/components/ui/buttons/OutlinedWhiteArrow
 
 const HomepageBanner: React.FC = () => {
   return (
-    <Box sx={{ width: '100%', height: 650, position: 'relative' }}>
+    <Box sx={{ width: '100%', height: { xs: 280, sm: 360, md: 480, lg: 650 }, position: 'relative' }}>
       <Box
         component="img"
         src="/banner.webp"
@@ -21,7 +21,7 @@ const HomepageBanner: React.FC = () => {
       <Box
         sx={{
           position: 'absolute',
-          top: 72,
+          top: { xs: 36, md: 72 },
           left: 0,
           right: 0,
           bottom: 0,
@@ -39,6 +39,7 @@ const HomepageBanner: React.FC = () => {
                 color: `${theme.palette.grey[50]} !important`,
                 textAlign: 'center',
                 lineHeight: '130%',
+                fontSize: { xs: 24, sm: 32, md: 40, lg: 76 },
               })}
             >
               Bridal shop with the possibility of individual tailoring
