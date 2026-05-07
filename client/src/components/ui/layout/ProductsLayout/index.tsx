e2273dc3afda2077ad80e10f758d799e59bdae05
berezenko04 <koffld@yahoo.com>
2026-05-07 21:55:58 +0100

    Add responsive to product layout grid

--- client/src/components/ui/layout/ProductsLayout/index.tsx
+++ client/src/components/ui/layout/ProductsLayout/index.tsx
@@ -1,13 +1,13 @@
-import { Divider, Grid, Pagination, Stack, SvgIconTypeMap } from "@mui/material";
+import { Divider, Grid, Pagination, Stack, SvgIconTypeMap } from '@mui/material';
 
 // components
-import ProductCard from "@/components/features/product/Card";
-import ProductCardSkeleton from "@/components/ui/loaders/skeletons/ProductCard";
-import EmptyState from "@/components/ui/EmptyState";
+import ProductCard from '@/components/features/product/Card';
+import ProductCardSkeleton from '@/components/ui/loaders/skeletons/ProductCard';
+import EmptyState from '@/components/ui/EmptyState';
 
 // types
-import { Product } from "@/api/products/products.types";
-import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { Product } from '@/api/products/products.types';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 type ProductsGridLayoutProps = {
   isLoading: boolean;
@@ -18,7 +18,7 @@ type ProductsGridLayoutProps = {
   onPageChange: (_: React.ChangeEvent<unknown>, newPage: number) => void;
   emptyStateTitle: string;
   emptyStateDescription: string;
-  emptyStateIcon: OverridableComponent<SvgIconTypeMap<object, "svg">>;
+  emptyStateIcon: OverridableComponent<SvgIconTypeMap<object, 'svg'>>;
   emptyStateAdditional?: React.ReactNode;
 };
 
@@ -37,17 +37,17 @@ const ProductsGridLayout: React.FC<ProductsGridLayoutProps> = ({
   return (
     <Stack gap={4}>
       {isLoading ? (
-        <Grid container spacing={4}>
+        <Grid container spacing={{ xs: 2, md: 4 }}>
           {Array.from({ length: 9 }).map((_, idx) => (
-            <Grid key={idx} size={{ xs: 4 }}>
+            <Grid key={idx} size={{ xs: 12, sm: 6, md: 4 }}>
               <ProductCardSkeleton />
             </Grid>
           ))}
         </Grid>
       ) : total > 0 ? (
-        <Grid container spacing={4}>
+        <Grid container spacing={{ xs: 2, md: 4 }}>
           {items.map((i) => (
-            <Grid key={i.id} size={{ xs: 4 }}>
+            <Grid key={i.id} size={{ xs: 12, sm: 6, md: 4 }}>
               <ProductCard variant="catalog" {...i} />
             </Grid>
           ))}
