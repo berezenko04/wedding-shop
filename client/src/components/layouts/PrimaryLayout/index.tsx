10d8e08f3f960e99808cf8887c18b6dab271e9f6
berezenko04 <koffld@yahoo.com>
2026-05-09 16:18:04 +0100

    Added padding for xs in PrimaryLayout

--- client/src/components/layouts/PrimaryLayout/index.tsx
+++ client/src/components/layouts/PrimaryLayout/index.tsx
@@ -1,13 +1,13 @@
-import { Stack } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Stack } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 
 // components
-import Header from "@/components/navigation/Header";
-import WarAlert from "@/components/navigation/WarAlert";
-import Footer from "@/components/navigation/Footer";
-import PrivacyRights from "@/components/navigation/Footer/PrivacyRights";
-import Breadcrumbs from "@/components/navigation/Breadcrumbs";
-import CustomContainer from "@/components/ui/layout/CustomContainer";
+import Header from '@/components/navigation/Header';
+import WarAlert from '@/components/navigation/WarAlert';
+import Footer from '@/components/navigation/Footer';
+import PrivacyRights from '@/components/navigation/Footer/PrivacyRights';
+import Breadcrumbs from '@/components/navigation/Breadcrumbs';
+import CustomContainer from '@/components/ui/layout/CustomContainer';
 
 const PrimaryLayout: React.FC = () => {
   return (
@@ -15,7 +15,7 @@ const PrimaryLayout: React.FC = () => {
       <WarAlert />
       <Header />
       <Breadcrumbs />
-      <Stack flexGrow={1} flexDirection="column" py={6}>
+      <Stack flexGrow={1} flexDirection="column" py={{ xs: 4, md: 6 }}>
         <CustomContainer>
           <Outlet />
         </CustomContainer>
