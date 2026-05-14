36da421526a7a298f7d5836516b49ec4e2bf2efb
berezenko04 <koffld@yahoo.com>
2026-05-14 17:25:43 +0100

    Add responsive to product rating section

--- client/src/components/features/product/Rating/index.tsx
+++ client/src/components/features/product/Rating/index.tsx
@@ -1,17 +1,17 @@
-import { Stack, Typography } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
+import { Stack, Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 
 // components
-import ProductRatingItem from "./Item";
+import ProductRatingItem from './Item';
 
 // api
-import ReviewsService from "@/api/reviews/reviews.service";
+import ReviewsService from '@/api/reviews/reviews.service';
 
 // types
-import { Rating } from "@/types/enums.types";
+import { Rating } from '@/types/enums.types';
 
 // icons
-import { Star } from "@mui/icons-material";
+import { Star } from '@mui/icons-material';
 
 type ProductRatingProps = {
   productId: string | undefined;
@@ -19,9 +19,9 @@ type ProductRatingProps = {
 
 const ProductRating: React.FC<ProductRatingProps> = ({ productId }) => {
   const { data: ratings } = useQuery({
-    queryKey: ["productRating", productId],
+    queryKey: ['productRating', productId],
     queryFn: async () => {
-      if (!productId) throw new Error("Product id is not provided");
+      if (!productId) throw new Error('Product id is not provided');
       return ReviewsService.getProductRatings(productId);
     },
     enabled: !!productId,
@@ -30,9 +30,13 @@ const ProductRating: React.FC<ProductRatingProps> = ({ productId }) => {
   if (!ratings) return null;
 
   return (
-    <Stack gap={6}>
+    <Stack gap={{ xs: 2, sm: 4, md: 6 }}>
       <Typography variant="h3">Rating & Reviews</Typography>
-      <Stack flexDirection="row" alignItems="center" gap={8}>
+      <Stack
+        flexDirection={{ xs: 'column', sm: 'row' }}
+        alignItems={{ xs: 'flex-start', sm: 'center' }}
+        gap={{ xs: 2, sm: 4, md: 8 }}
+      >
         <Stack>
           <Typography fontSize={64} lineHeight="100%" variant="medium">
             {ratings.averageRating.toFixed(1)}
@@ -40,13 +44,13 @@ const ProductRating: React.FC<ProductRatingProps> = ({ productId }) => {
           <Stack gap={1}>
             <Stack flexDirection="row" alignItems="center">
               {[...Array(Math.round(ratings.averageRating))].map((_, idx) => (
-                <Star key={idx} sx={{ color: "yellow.500" }} />
+                <Star key={idx} sx={{ color: 'yellow.500' }} />
               ))}
             </Stack>
             <Typography textTransform="uppercase">{ratings.totalVotes} Ratings</Typography>
           </Stack>
         </Stack>
-        <Stack gap={1.5}>
+        <Stack gap={1.5} sx={{ width: '100%' }}>
           {Object.entries(ratings.distribution || {}).map(([ratingMark, ratingCount]) => (
             <ProductRatingItem
               key={ratingMark}
