d0dcea3b6cc4e1f40ffb132b56b2379ae4f2dd4c
berezenko04 <koffld@yahoo.com>
2026-05-18 19:24:32 +0100

    Remove dead imports and exports

--- client/src/components/layouts/AppLayout/index.tsx
+++ client/src/components/layouts/AppLayout/index.tsx
@@ -1,21 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { Outlet } from "react-router";
-import { useSelector } from "react-redux";
+import { useQuery } from '@tanstack/react-query';
+import { Outlet } from 'react-router';
+import { useSelector } from 'react-redux';
 
 // api
-import WishlistService from "@/api/wishlist/wishlist.service";
+import WishlistService from '@/api/wishlist/wishlist.service';
 
 // redux
-import { authSelector } from "@/redux/auth/auth.selectors";
+import { authSelector } from '@/redux/auth/auth.selectors';
 
 // constants
-import { PAGE_LIMIT } from "@/constants";
+import { PAGE_LIMIT } from '@/constants';
 
-export const AppLayout = () => {
+const AppLayout = () => {
   const { isAuth } = useSelector(authSelector);
 
   useQuery({
-    queryKey: ["wishlist", { page: 1, limit: PAGE_LIMIT }],
+    queryKey: ['wishlist', { page: 1, limit: PAGE_LIMIT }],
     queryFn: () => WishlistService.getAll({ page: 1, limit: PAGE_LIMIT }),
     enabled: isAuth,
     refetchOnWindowFocus: false,
