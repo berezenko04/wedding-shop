a6e2ff10ea01931d7d404d3af0672374c93502a8
berezenko04 <koffld@yahoo.com>
2026-05-18 20:23:20 +0100

    Remove unused component from layouts

--- client/src/components/layouts/HomeLayout/index.tsx
+++ client/src/components/layouts/HomeLayout/index.tsx
@@ -1,16 +1,14 @@
-import { Stack } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Stack } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 
 // components
-import Header from "@/components/navigation/Header";
-import WarAlert from "@/components/navigation/WarAlert";
-import Footer from "@/components/navigation/Footer";
-import PrivacyRights from "@/components/navigation/Footer/PrivacyRights";
+import Header from '@/components/navigation/Header';
+import Footer from '@/components/navigation/Footer';
+import PrivacyRights from '@/components/navigation/Footer/PrivacyRights';
 
 const HomeLayout: React.FC = () => {
   return (
     <Stack minHeight="100svh">
-      <WarAlert />
       <Header />
       <Stack flexGrow={1} flexDirection="column">
         <Outlet />
