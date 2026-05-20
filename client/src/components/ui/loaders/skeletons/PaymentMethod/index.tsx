ff678d13702d4d5a331bd3de87c3625ea5925c6f
berezenko04 <koffld@yahoo.com>
2026-05-20 13:08:23 +0100

    Make PaymentMethodSkeleton responsive on mobile

--- client/src/components/ui/loaders/skeletons/PaymentMethod/index.tsx
+++ client/src/components/ui/loaders/skeletons/PaymentMethod/index.tsx
@@ -7,11 +7,11 @@ const PaymentMethodSkeleton: React.FC = () => {
   return (
     <OutlinedBlock>
       <Stack flexDirection="row" justifyContent="space-between" gap={4} alignItems="flex-start">
-        <Stack flexDirection="row" alignItems="center" gap={2}>
-          <Skeleton variant="rounded" animation="wave" width={60} height={40} />
-          <Stack>
-            <Skeleton variant="text" animation="wave" width={360} height={24} />
-            <Skeleton variant="text" animation="wave" width={280} height={24} />
+        <Stack flexDirection="row" alignItems="center" gap={2} sx={{ minWidth: 0, flex: 1 }}>
+          <Skeleton variant="rounded" animation="wave" width={60} height={40} sx={{ flexShrink: 0 }} />
+          <Stack sx={{ minWidth: 0, flex: 1 }}>
+            <Skeleton variant="text" animation="wave" height={24} sx={{ width: { xs: '100%', sm: 360 } }} />
+            <Skeleton variant="text" animation="wave" height={24} sx={{ width: { xs: '75%', sm: 280 } }} />
           </Stack>
         </Stack>
       </Stack>
